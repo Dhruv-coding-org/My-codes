1328e54a2e049c045e7021ff59003a672a45540e
Dhruv-coding-org <dhruv.adhikari68@gmail.com>
2025-11-26 18:16:46 +0530

    Add files via upload

---
+++ Dhruv codes/project/my/index.js
@@ -0,0 +1,25 @@
+let grey= document.querySelector(".grey")
+let blue= document.querySelector(".blue")
+let white= document.querySelector(".white")
+let yellow= document.querySelector(".yellow")
+
+grey.addEventListener("click",()=>{
+    
+    document.body.style.backgroundColor= "grey";
+
+})
+blue.addEventListener("click",()=>{
+    
+    document.body.style.backgroundColor= "blue";
+
+})
+white.addEventListener("click",()=>{
+    
+    document.body.style.backgroundColor= "white";
+
+})
+yellow.addEventListener("click",()=>{
+    
+    document.body.style.backgroundColor= "yellow";
+
+})
